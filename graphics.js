2086397b66ce1fa06b2ea118a8924ff9ac5c4201
Juan Camilo Florez <jcflorezv@unal.edu.co>
2022-05-17 10:32:46 -0500

    added shader varying

--- graphics.js
+++ graphics.js
@@ -38,11 +38,10 @@ function main() {
   var program = createProgram(gl, vertexShader, fragmentShader);
 
   var positionAttribLocation = gl.getAttribLocation(program, 'a_position');
-  var resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
-  var colorUniformLocation = gl.getUniformLocation(program, 'u_color');
+
 
   var positionBuffer = gl.createBuffer();
-  
+
   // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
@@ -57,21 +56,87 @@ function main() {
 
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  // Clear the canvas
-  // gl.clearColor(0, 0, 0, 0);
-  // gl.clear(gl.COLOR_BUFFER_BIT);
-
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
-
   gl.enableVertexAttribArray(positionAttribLocation);
 
+  addSlider(gl, function () {
+    drawBasicExample(gl, program, this.value);
+  });
+
+  // drawRandomSquares(gl, program);
+  drawBasicExample(gl, program, 0);
+}
+
+function clearCanvas(gl) {
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function addSlider(gl, callback) {
+  let slider = document.createElement('input');
+  slider.setAttribute("type", "range");
+  slider.setAttribute("value", gl.canvas.width/2);
+  slider.setAttribute("min", 0);
+  slider.setAttribute("max", gl.canvas.width);
+  slider.oninput = callback;
+  document.body.append(slider);
+}
+
+function drawBasicExample(gl, program, mv) {
+  mv = parseInt(mv);
+  var resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
+  var colorUniformLocation = gl.getUniformLocation(program, 'u_color');
+  var sideUniformLocation = gl.getUniformLocation(program, 'u_side');
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+  gl.uniform4f(colorUniformLocation, Math.random(), 0.5, 0.5, 1);
+
+  gl.uniform1f(sideUniformLocation, true);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0 + mv, 0 + mv,
+      100 + mv, 0 + mv,
+      100 + mv, 70 + mv,
+    ]),
+    gl.STATIC_DRAW
+  )
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+
+  // ---
+  gl.uniform1f(sideUniformLocation, false);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      100 + mv, 70 + mv,
+      0 + mv, 70 + mv,
+      0 + mv, 0 + mv
+    ]),
+    gl.STATIC_DRAW
+  )
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+}
+
+function drawSemiRectangle() {
+
+}
+
+function drawRandomSquares(gl, program) {
+  var resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
+  var colorUniformLocation = gl.getUniformLocation(program, 'u_color');
   for (ii = 0; ii < 50; ii++) {
-    
-    var ow = Math.random()*canvas.width;
-    var oh = Math.random()*canvas.height;
-    var sw = Math.random()*(canvas.width - ow);
-    var sh = Math.random()*(canvas.height - oh);
+    var ow = Math.random() * gl.canvas.width;
+    var oh = Math.random() * gl.canvas.height;
+    var sw = Math.random() * (gl.canvas.width - ow);
+    var sh = Math.random() * (gl.canvas.height - oh);
     var r = Math.random();
     var g = Math.random();
     var b = Math.random();
@@ -79,15 +144,14 @@ function main() {
     gl.uniform4f(colorUniformLocation, r, g, b, 1);
     // console.log(l1 + " - " + l2 + " .. " + r + " " + g + " " + b);
     var positions = [
-      ow,       oh,
-      ow + sw,  oh,
-      ow + sw,  oh + sh,
-      ow + sw,  oh + sh,
-      ow,       oh + sh,
-      ow,       oh
+      ow, oh,
+      ow + sw, oh,
+      ow + sw, oh + sh,
+      ow + sw, oh + sh,
+      ow, oh + sh,
+      ow, oh
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
 
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
