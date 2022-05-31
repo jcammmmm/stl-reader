22028b5d0d84a3e2bd09591ab916683d58f59d61
Juan Camilo Florez <jcflorezv@unal.edu.co>
2022-05-30 22:54:05 -0500

    added rotation controls

--- geom2d.js
+++ geom2d.js
@@ -1,9 +1,12 @@
 var vertShader = `
   attribute vec4 a_position;
   uniform vec2 u_translation;
+  uniform vec2 u_rotation;
 
   void main() {
-    gl_Position = a_position + vec4(u_translation, 0, 0);
+    vec2 rot_position = vec2(a_position.x*u_rotation.y - a_position.y*u_rotation.x,
+                             a_position.x*u_rotation.x + a_position.y*u_rotation.y);
+    gl_Position = vec4(rot_position + u_translation, 0, 1);
   }
 `
 
@@ -30,12 +33,15 @@ function main() {
   
   gl.useProgram(program);
   var unifLocColor = gl.getUniformLocation(program, 'u_color');
-  var unifLocTrans = gl.getUniformLocation(program, 'u_translation')
+  var unifLocTrans = gl.getUniformLocation(program, 'u_translation');
+  var unifLocRotat = gl.getUniformLocation(program, 'u_rotation');
   
   setupSlider(0, gl.canvas.height);
   setupSlider(1, gl.canvas.width);
+  setupSliderTurn(1);
   
   let translation = [0, 0];
+  let rotation = [0, 1];
   
   let shape = buildF(0, 0, 0.05, 8);
   set2dShape(gl, shape);
@@ -43,6 +49,7 @@ function main() {
   function drawScene() {
     gl.uniform4f(unifLocColor, 0.5, 0.5, 1 , 1);
     gl.uniform2f(unifLocTrans, translation[0], translation[1]);
+    gl.uniform2f(unifLocRotat, rotation[0], rotation[1]);
     gl.drawArrays(gl.TRIANGLES, 0, shape.length/pointSize);
   }
 
@@ -52,6 +59,16 @@ function main() {
       translation[axis] = coord/max*2 - 1;
       drawScene();
     });
+  }
+
+  function setupSliderTurn(turns) {
+    let precision = 1000;
+    configureSlider(2*turns*Math.PI*precision, function(event) {
+      let rad = parseInt(event.target.value);
+      // TODO: The rotation also is scaling the object
+      rotation = [Math.sin(rad/precision), Math.cos(rad/precision)];
+      drawScene();
+    })
   }
 
   function buildRectangle(x0, y0, width, height) {
